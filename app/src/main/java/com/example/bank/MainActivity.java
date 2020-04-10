10870b61ef974129d32c6e9e3df195045902c544
IleJok <ilkka.jokela@gmail.com>
2020-04-10 14:49:29 +0300

    First version of Bank class

--- app/src/main/java/com/example/bank/MainActivity.java
+++ app/src/main/java/com/example/bank/MainActivity.java
@@ -10,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*
+        Bank bank1 = new Bank("Nordea", "Pankkikatu", "Suomi", "FINOR");
+        Bank bank2 = new Bank("Sampo", "Pankkitie", "Suomi", "FISamp");
+
+        System.out.println("Ensimmäisen pankin tiedot: "+ bank1.toString());
+        System.out.println("Toisen pankin tiedot: "+ bank2.toString());
+        System.out.println("Onko oliot yhtäläiset (pitäisi olla false): "+ bank1.equals(bank2));
+        System.out.println("Onko oliot yhtäläiset (pitäisi olla true): "+ bank1.equals(bank1));
+        */
     }
 }
